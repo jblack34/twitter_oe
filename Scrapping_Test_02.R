3e21ae722a3b5d7095cbcec269b86d49d970b1b7
Jakob <jakob.schwarz@student.uibk.ac.at>
2024-06-11 02:34:33 +0200

    Zweiter html scrapping test, (non funktional)

---
+++ Scrapping_Test_02.R
@@ -0,0 +1,32 @@
+install.packages("rvest")
+install.packages("dplyr")
+library(rvest)
+library(dplyr)
+# Definiere die URL der Website
+url <- "https://www.meineabgeordneten.at/Abgeordnete?gremien%5b%5d=1"
+
+# Lese die HTML-Seite
+page <- read_html(url)
+
+# Extrahiere die Namen
+names <- page %>% html_nodes(".abgeordneter .name") %>% html_text(trim = TRUE)
+
+# Extrahiere das Alter
+ages <- page %>% html_nodes(".abgeordneter .name") %>% html_text(trim = TRUE)
+ages <- sub(".*, (\\d+),.*", "\\1", ages) # Extrahiere nur die Altersangabe
+
+# Extrahiere die Parteizugehörigkeit
+parties <- page %>% html_nodes(".abgeordneter .partei") %>% html_text(trim = TRUE)
+
+# Erstelle einen DataFrame mit den extrahierten Daten
+politicians1 <- data.frame(
+  Name = names,
+  Alter = ages,
+  Partei = parties,
+  stringsAsFactors = FALSE
+)
+
+# Zeige die extrahierten Daten an
+print(politicians1)
+View(politicians1)
+save(politicians1)
